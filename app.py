86152232456dc11f631756a6cee08bfa279af57e
cristianjos <129961949+cristianjos1307@users.noreply.github.com>
2024-04-01 12:05:43 -0300

    subindo

---
+++ app.py
@@ -0,0 +1,32 @@
+print("Bem vindo ao Programa Expresso")
+
+
+print ("Programa Expresso")
+
+
+print("1-Cadastrar restaurante")
+
+
+print("2-Listar restaurante")
+
+
+print("3-Ativar restaurante")
+
+
+print("4-Sair do aplicativo")
+
+OPCAO_DIGITADA = int(input("Digite a opção desejada"))
+if(OPCAO_DIGITADA == 1):
+    print("Você escolheu cadastrar restaurante")
+
+elif(OPCAO_DIGITADA == 2):
+    print("Você escolheu listar restaurante")
+
+elif(OPCAO_DIGITADA == 3):
+    print("Você escolheu Ativar restaurante")
+
+elif(OPCAO_DIGITADA == 4):
+        print("Você escolheu Sair do aplicativo")
+
+else:
+    print("Essa opção não existe, escolha as opcções existentes!")        
